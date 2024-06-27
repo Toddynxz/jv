12bf3d5ed0be26cf76053b6d9855ea654cdc0d2a
Bryan <168776218+Toddynxz@users.noreply.github.com>
2024-06-27 17:00:41 -0300

    Add files via upload

---
+++ pingpong.js
@@ -0,0 +1,11 @@
+function setup(){
+    //aqui vou criar minha "mesa"
+    createCanvas(600,400);
+}
+
+function draw(){
+    //aqui vou por a cor da "mesa"
+    //A cor da mesa é em rgb
+    background(255,255,255); 
+}
+
