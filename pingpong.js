ec2ece7fe1d32dd47585d0c710b093a4ad19bc5a
Bryan <168776218+Toddynxz@users.noreply.github.com>
2024-08-01 16:24:11 -0300

    a

--- pingpong.js
+++ pingpong.js
@@ -2,7 +2,7 @@
 let xBolinha = 300;
 let yBolinha = 200;
 let tamBolinha = 25;
-let raioBolinha = tamanhoBolinha/2;a
+let raioBolinha = tamanhoBolinha/2;
 
 //config velocidade bolinha
 let xvelocidadeBolinha = 6;
